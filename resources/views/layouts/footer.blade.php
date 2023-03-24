f32f738bcf20547e3981b20a7fb2825e250653f5
abdulwhab241 <abdulwhab241@gmail.com>
2023-03-24 01:02:01 -0400

    fix theme

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="bg-white p-4">
+<div class="row">
+    <div class="col-md-6">
+        <div class="text-center text-md-right">
+            <p class="mb-0"> &copy; جميع الحقوق محفوظة<span id="copyright">
+                    <script>
+                        document.getElementById('copyright').appendChild(document
+                            .createTextNode(new Date().getFullYear()))
+                    </script>
+                </span>. <a href="https://api.whatsapp.com/send/?phone=967770101198&text&type=phone_number&app_absent=0"> برنامج عبدالوهاب لإدارة المدارس</a></p>
+        </div>
+    </div>
+</div>
+</footer>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,68 @@
+<!-- Title -->
+<title>@yield("title")</title>
+
+<!-- Favicon -->
+{{-- <link rel="shortcut icon" href="{{ URL::asset('/assets/images/favicon.ico') }}" type="/image/x-icon" /> --}}
+
+<!-- Font -->
+{{-- <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900"> --}}
+    {{-- <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/> --}}
+    {{-- <link href="{{ URL::asset('css/wizard.css') }}" rel="stylesheet" id="bootstrap-css"> --}}
+    @yield('css')
+<!--- Style css -->
+{{-- <link href="{{ URL::asset('/assets/style.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/style.css">
+
+<!--- Style css -->
+{{-- @if (App::getLocale() == 'en')
+    <link href="{{ URL::asset('/assets/css/ltr.css') }}" rel="stylesheet">
+@else --}}
+    {{-- <link href="{{ URL::asset('/assets/css/rtl.css') }}" rel="stylesheet"> --}}
+{{-- @endif --}}
+
+<!-- Bootstrap 3.3.4 -->
+{{-- <link href="{{ URL::asset('/assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<!-- Ionicons 2.0.0 -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- Theme style -->
+{{-- <link href="{{ URL::asset('/assets/dist/css/AdminLTE.min.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
+<!-- AdminLTE Skins. Choose a skin from the css/skins
+     folder instead of downloading all of them to reduce the load. -->
+{{-- <link href="{{ URL::asset('/assets/dist/css/skins/_all-skins.min.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
+<!-- iCheck -->
+{{-- <link href="{{ URL::asset('/assets/plugins/iCheck/flat/blue.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/iCheck/flat/blue.css">
+<!-- Morris chart -->
+{{-- <link href="{{ URL::asset('/assets/plugins/morris/morris.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/morris/morris.css">
+<!-- jvectormap -->
+{{-- <link href="{{ URL::asset('/assets/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+<!-- Date Picker -->
+{{-- <link href="{{ URL::asset('/assets/plugins/datepicker/datepicker3.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/datepicker/datepicker3.css">
+<!-- Daterange picker -->
+{{-- <link href="{{ URL::asset('/assets/plugins/daterangepicker/daterangepicker-bs3.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker-bs3.css">
+<!-- bootstrap wysihtml5 - text editor -->
+{{-- <link href="{{ URL::asset('/assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+{{-- <link href="{{ URL::asset('/assets/dist/fonts/fonts-fa.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/dist/fonts/fonts-fa.css">
+{{-- <link href="{{ URL::asset('/assets/dist/css/bootstrap-rtl.min.css') }}" rel="stylesheet"> --}}
+
+{{-- <link href="{{ URL::asset('/assets/dist/css/rtl.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/dist/css/bootstrap-rtl.min.css">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
